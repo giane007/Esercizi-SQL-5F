1a6547fa1a97eafb65547e0b3727a2a846f4b404
IL GIANE <150336285+giane007@users.noreply.github.com>
2025-10-16 18:13:27 +0200

    Add files via upload

---
+++ Es.3/InserisciDipendenti.sql
@@ -0,0 +1,3 @@
+insert into dipendenti(ID_dipendente,cognome,nome,data_nascita,cap, citta, anzianita)
+values (4356,'Brado','Ric','1010-10-10','69104','Belfast','89'),
+(8902,'Ringo','Giacomo','2001-09-11','45678','Firenze','65');
---
+++ Es.3/InserisciReparti.sql
@@ -0,0 +1,3 @@
+insert into reparti(ID_reparto,nome_reparto,cod_reparto)
+values (345,'Panificati',900),
+(400,'Cucina',1100);
