dcc0cae41e28954e7595afd74390ff7545bebc75
IL GIANE <150336285+giane007@users.noreply.github.com>
2025-10-16 18:13:48 +0200

    Add files via upload

---
+++ Libri/InserisciLibri.sql
@@ -0,0 +1,3 @@
+insert into libri(isbn,titolo,autore,argomento,editore,anno,edizione)
+values ('nfgdh','Fosca','Ugo Tarchetti','Dualismo','Giovanni',1850,1972),
+('fjgjd','Divina Commedia','Dante Alighieri','Ascesa','Pascoli',1320,1290);
