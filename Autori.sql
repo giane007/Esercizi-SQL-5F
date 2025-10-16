2e4641d4dc1d7d86320c45152c88f80024ddbe8f
IL GIANE <150336285+giane007@users.noreply.github.com>
2025-10-16 18:12:27 +0200

    Delete Autori.sql

--- Autori.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE autori (
-    codice INT AUTO_INCREMENT PRIMARY KEY,
-    cf CHAR(16) NOT NULL UNIQUE,
-    cognome VARCHAR(50) NOT NULL,
-    nome VARCHAR(50) NOT NULL,
-    città VARCHAR(50)
-);
